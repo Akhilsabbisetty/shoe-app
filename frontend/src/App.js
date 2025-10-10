40684814474266fa4fc6fa4342d581984384ba53
root <sabbisettyakhil22@gmail.com>
2025-10-10 01:53:05 +0000

    Unknown

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>🥿 Shoe Frontend App</h1>
+      <p>Frontend is working correctly!</p>
+    </div>
+  );
+}
+
+export default App;
+
