d12e8e3ffa5f7d45aa7378f46ad91a72d2e83924
Akhil Sabbisetty <sabbisettyakhil22@gmail.com>
2025-10-12 18:18:21 +0530

    Fix to K8s in jenkins file

--- backend/src/main/java/com/shoes/ShoeApplication.java
+++ backend/src/main/java/com/shoes/ShoeApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShoeBackendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ShoeBackendApplication.class, args);
+        SpringApplication.run(ShoeApplication.class, args);
     }
 }
