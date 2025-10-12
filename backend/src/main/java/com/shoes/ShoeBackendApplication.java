84a896c5d2a4ddf4b3c442597dc0eabb314ab341
root <sabbisettyakhil22@gmail.com>
2025-10-12 13:25:06 +0000

    maven

--- backend/src/main/java/com/shoes/ShoeBackendApplication.java
+++ backend/src/main/java/com/shoes/ShoeBackendApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoeApplication {
+public class ShoeBackendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ShoeApplication.class, args);
+        SpringApplication.run(ShoeBackendApplication.class, args);
     }
 }
